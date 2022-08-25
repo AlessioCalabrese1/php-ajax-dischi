23228218aa86ff52985d1b6eab0c0f63a66ae79e
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-08-25 17:09:51 +0200

    Bonus

--- Second version/script.js
+++ Second version/script.js
@@ -2,14 +2,21 @@ var app = new Vue({
     el: '#app',
     data: {
       discs : [],
+      genres : [
+        "Rock",
+        "Pop",
+        "Jazz",
+        "Metal",
+      ],
+      genreSelected: "All"
     },
 
     methods: {
         getDiscs(){
-            axios.get('http://localhost/php-ajax-dischi/database/controller.php')
+            console.log(this.genreSelected)
+            axios.get(`http://localhost/php-ajax-dischi/database/controller.php?genre=${this.genreSelected}`)
             .then(result => {
                 this.discs = result.data.result;
-                console.log(this.discs);
             }) 
         }
     },
--- database/controller.php
+++ database/controller.php
@@ -1,7 +1,30 @@
 <?php 
     include __DIR__ . "/db.php"; 
+    $genreSelected = $_GET["genre"];
+    function filtering($element){
+        if ($element["genre"] == $_GET["genre"]) {
+            return $element;
+        }
+    };
+
+
+    if($genreSelected == "All"){
+        $filteredDiscs = $discs;
+    }elseif($genreSelected == "Pop"){
+        $filteredDiscs = $discs;
+        $filteredDiscs = array_filter($filteredDiscs, "filtering");
+    }elseif($genreSelected == "Rock"){
+        $filteredDiscs = $discs;
+        $filteredDiscs = array_filter($filteredDiscs, "filtering");
+    }elseif($genreSelected == "Jazz"){
+        $filteredDiscs = $discs;
+        $filteredDiscs = array_filter($filteredDiscs, "filtering");
+    }elseif($genreSelected == "Metal"){
+        $filteredDiscs = $discs;
+        $filteredDiscs = array_filter($filteredDiscs, "filtering");
+    }
 
     header('Content-Type: application/json');
     echo json_encode(["success" => true,
-                    "result" => $discs]);
+                    "result" => $filteredDiscs]);
 ?>
